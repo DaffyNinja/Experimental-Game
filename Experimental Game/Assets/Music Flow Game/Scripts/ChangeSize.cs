8dc6d41c1ceb768cc36fee8a89d81965c855ef8d
Marcus Day <110797@student.sae.edu.au>
2015-12-21 11:24:54 +1100

    Started work on the music flow idea

---
+++ Experimental Game/Assets/Music Flow Game/Scripts/ChangeSize.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSize : MonoBehaviour {
+
+	public float growSize;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.Return))
+		{
+			transform.localScale = Vector3.Lerp(transform.localScale,new Vector3(0.5f,growSize,0.5f),speed);
+
+
+
+		}
+	
+	}
+}
