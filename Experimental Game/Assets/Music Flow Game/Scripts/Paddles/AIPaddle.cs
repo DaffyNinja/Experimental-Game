3f2df4fa4495d4c9656c9c731fc2d3397cb44216
Marcus <mdaygmdesigner@gmail.com>
2015-12-23 11:05:34 +1100

    Added new ball script

---
+++ Experimental Game/Assets/Music Flow Game/Scripts/Paddles/AIPaddle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIPaddle : MonoBehaviour {
+
+    public float speed;
+
+
+    GameObject ballOBJ;
+
+	// Use this for initialization
+	void Start () 
+    {
+        ballOBJ = GameObject.FindGameObjectWithTag("Ball");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate(0, ballOBJ.transform.position.y * Time.deltaTime * speed, 0);
+	
+	}
+}
