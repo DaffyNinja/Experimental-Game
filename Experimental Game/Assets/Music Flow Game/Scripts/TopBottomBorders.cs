e1c172c872948ebf51108237a79b66e0647a19b9
Marcus <mdaygmdesigner@gmail.com>
2016-01-02 10:53:22 +1100

    Made top bottom border script to help with position

---
+++ Experimental Game/Assets/Music Flow Game/Scripts/TopBottomBorders.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopBottomBorders : MonoBehaviour
+{
+
+    public bool isTop;
+    //public bool isBottom;
+
+
+    Vector3 worldPoint;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Top
+        if (isTop)
+        {
+            worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(350, 625, 0));
+
+            gameObject.transform.position = worldPoint;
+        }
+        else
+        {
+            worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(350, 0, 0));
+
+            gameObject.transform.position = worldPoint;
+        }
+
+
+
+    }
+}
